6cb0706c3be4e8717d7709d2d389a1e674ed8cef
Wonho-mis <37608231+six6929@users.noreply.github.com>
2018-11-26 15:10:35 +0900

    Add files via upload

---
+++ Spring/07_BoardService_Log/src/main/java/com/spring/biz/board/BoardServiceClient.java
@@ -0,0 +1,47 @@
+package com.spring.biz.board;
+
+import java.util.List;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BoardServiceClient {
+	public static void main(String[] args) {
+		//1. 스프링컨테이너 가동
+		AbstractApplicationContext container 
+		 = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		//2. 필요한 객체 사용
+		//Board 테이블에 데이터 입력,수정,삭제,조회
+		BoardService boardService =
+				(BoardService) container.getBean("boardService");
+		System.out.println("--- 스프링컨테이너 구동완료 ---");
+		
+		//2-1. 입력
+		BoardVO vo = new BoardVO();
+		vo.setTitle("테스트2");
+		vo.setWriter("홍길동");
+		vo.setContent("테스트2 내용");
+		boardService.insertBoard(vo);
+		
+		
+		//2-2. 수정
+		/*vo.setSeq(2);
+		vo.setTitle("테스트2-수정");
+		vo.setContent("테스트2 내용");
+		boardService.updateBoard(vo);*/
+		
+		
+/*		//2-3. 삭제
+		vo.setSeq(3);
+		boardService.deleteBoard(vo);*/
+		
+		//2-4. 글 목록 조회
+		List<BoardVO> boardList = boardService.getBoardList(vo);
+		for (BoardVO board : boardList) {
+			System.out.println(board.toString());
+		}
+		//3. 컨테이너 종료(close)
+		container.close();
+	}
+}
